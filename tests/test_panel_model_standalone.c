a911f738b557ad3e62f432596fb37f41f98507fc
roccolate <wroque@gmail.com>
2026-07-13 14:57:28 -0600

    test: cover panel menu and permission-safe activation

--- tests/test_panel_model_standalone.c
+++ tests/test_panel_model_standalone.c
@@ -17,6 +17,11 @@ int main(void) {
     uint32_t states[PANEL_APP_WINDOW_CAP] = {0};
     char clock_text[9];
 
+    CHECK(panel_target_at(PANEL_MENU_BUTTON_X, PANEL_ITEM_Y) ==
+          PANEL_TARGET_MENU);
+    CHECK(panel_target_at(PANEL_MENU_BUTTON_X + PANEL_MENU_BUTTON_W - 1,
+                          PANEL_ITEM_Y + PANEL_ITEM_H - 1) ==
+          PANEL_TARGET_MENU);
     CHECK(panel_target_at(panel_app_x(0), PANEL_ITEM_Y) == 0);
     CHECK(panel_target_at(panel_app_x(3) + PANEL_APP_W - 1,
                           PANEL_ITEM_Y + PANEL_ITEM_H - 1) == 3);
@@ -26,6 +31,19 @@ int main(void) {
     CHECK(panel_target_at(PANEL_CLOCK_X + PANEL_CLOCK_W,
                           PANEL_ITEM_Y) == PANEL_TARGET_NONE);
     CHECK(panel_target_at(10, PANEL_ITEM_Y - 1) == PANEL_TARGET_NONE);
+
+    for (int i = 0; i < PANEL_APP_COUNT; i++) {
+        CHECK(panel_menu_item_at(PANEL_MENU_ITEM_X,
+                                 panel_menu_item_y(i)) == i);
+        CHECK(panel_menu_item_at(PANEL_MENU_ITEM_X + PANEL_MENU_ITEM_W - 1,
+                                 panel_menu_item_y(i) +
+                                     PANEL_MENU_ITEM_H - 1) == i);
+    }
+    CHECK(panel_menu_item_at(PANEL_MENU_ITEM_X - 1,
+                             panel_menu_item_y(0)) == -1);
+    CHECK(panel_menu_item_at(PANEL_MENU_ITEM_X,
+                             panel_menu_item_y(0) +
+                                 PANEL_MENU_ITEM_H) == -1);
 
     CHECK(panel_visual_state(states, 0) == PANEL_VISUAL_CLOSED);
     CHECK(panel_app_visual_state(states, 0, 0) == PANEL_VISUAL_CLOSED);
@@ -54,6 +72,21 @@ int main(void) {
     states[1] = 0;
     states[2] = PANEL_WINDOW_STATE_FOCUSED;
     CHECK(panel_pick_window(states, 3) == 0);
+
+    CHECK(panel_activation_action(states, 0, 0) == PANEL_ACTION_LAUNCH);
+    CHECK(panel_activation_action(states, 0, 1) == PANEL_ACTION_WAIT);
+    states[0] = 0;
+    CHECK(panel_activation_action(states, 1, 1) == PANEL_ACTION_FOCUS);
+    states[0] = PANEL_WINDOW_STATE_MINIMIZED;
+    CHECK(panel_activation_action(states, 1, 1) == PANEL_ACTION_RESTORE);
+    states[0] = PANEL_WINDOW_STATE_FOCUSED;
+    CHECK(panel_activation_action(states, 1, 1) == PANEL_ACTION_NONE);
+    states[0] = PANEL_WINDOW_STATE_FOCUSED;
+    states[1] = 0;
+    CHECK(panel_activation_action(states, 2, 2) == PANEL_ACTION_FOCUS);
+    CHECK(panel_pick_window(states, 2) == 1);
+    states[1] = PANEL_WINDOW_STATE_MINIMIZED;
+    CHECK(panel_activation_action(states, 2, 2) == PANEL_ACTION_RESTORE);
 
     panel_format_uptime(0, clock_text);
     CHECK(strcmp(clock_text, "00:00:00") == 0);
