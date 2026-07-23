a0aa8bb4f8d74f7d85ef528e231b51f46e938e3a
roccolate <wroque@gmail.com>
2026-07-23 12:30:40 -0600

    Use public layouts for system information syscalls

--- kernel/syscall_info.c
+++ kernel/syscall_info.c
@@ -2,33 +2,29 @@
 
 #include <stdint.h>
 
+#include "include/armonios/abi/process.h"
+#include "include/armonios/abi/system.h"
 #include "kernel/kstring.h"
 #include "kernel/mm/pmm.h"
 #include "kernel/sched/sched.h"
 #include "kernel/syscall_helpers.h"
 #include "kernel/timer/timer.h"
 
-typedef struct {
-    uint32_t pid;
-    uint32_t state;
-    char name[16];
-} syscall_proc_entry_t;
-
 int64_t sys_meminfo(process_t *process, uint64_t info_ptr) {
-    uint64_t info[2];
+    arm_meminfo_t info;
 
-    info[0] = pmm_total_count();
-    info[1] = pmm_free_count();
-    return sys_copy_to_user(process, info_ptr, info, sizeof(info));
+    info.total_pages = pmm_total_count();
+    info.free_pages = pmm_free_count();
+    return sys_copy_to_user(process, info_ptr, &info, sizeof(info));
 }
 
 int64_t sys_timeinfo(process_t *process, uint64_t info_ptr) {
-    uint64_t info[3];
+    arm_timeinfo_t info;
 
-    info[0] = timer_ticks();
-    info[1] = sched_ticks();
-    info[2] = sched_quantums();
-    return sys_copy_to_user(process, info_ptr, info, sizeof(info));
+    info.timer_ticks = timer_ticks();
+    info.scheduler_ticks = sched_ticks();
+    info.scheduler_quantums = sched_quantums();
+    return sys_copy_to_user(process, info_ptr, &info, sizeof(info));
 }
 
 int64_t sys_proclist(process_t *process, uint64_t entries_ptr,
@@ -44,7 +40,7 @@ int64_t sys_proclist(process_t *process, uint64_t entries_ptr,
     }
 
     status = sys_user_buf_out(process, entries_ptr,
-                              max_entries * sizeof(syscall_proc_entry_t));
+                              max_entries * sizeof(arm_process_entry_t));
     if (status != 0) {
         return status;
     }
@@ -52,7 +48,7 @@ int64_t sys_proclist(process_t *process, uint64_t entries_ptr,
     for (uint32_t i = 0; i < PROCESS_MAX_PROCESSES && written < max_entries;
          i++) {
         const process_t *slot = process_at(i);
-        syscall_proc_entry_t entry;
+        arm_process_entry_t entry;
         const char *name;
         uint32_t j;
 
@@ -73,7 +69,7 @@ int64_t sys_proclist(process_t *process, uint64_t entries_ptr,
         }
 
         kmemcpy((void *)(uintptr_t)(
-                    entries_ptr + written * sizeof(syscall_proc_entry_t)),
+                    entries_ptr + written * sizeof(arm_process_entry_t)),
                 &entry, sizeof(entry));
         written++;
     }
