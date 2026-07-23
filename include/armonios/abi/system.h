1f937cc64c6840052e14d259d1ab4f4cb71d9990
roccolate <wroque@gmail.com>
2026-07-23 12:35:53 -0600

    Lock public system information field offsets

--- include/armonios/abi/system.h
+++ include/armonios/abi/system.h
@@ -1,6 +1,7 @@
 #ifndef ARMONIOS_INCLUDE_ARMONIOS_ABI_SYSTEM_H
 #define ARMONIOS_INCLUDE_ARMONIOS_ABI_SYSTEM_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 typedef struct {
@@ -15,6 +16,16 @@ typedef struct {
 } arm_timeinfo_t;
 
 _Static_assert(sizeof(arm_meminfo_t) == 16, "ABI drift: arm_meminfo_t");
+_Static_assert(offsetof(arm_meminfo_t, total_pages) == 0,
+               "ABI drift: meminfo total_pages offset");
+_Static_assert(offsetof(arm_meminfo_t, free_pages) == 8,
+               "ABI drift: meminfo free_pages offset");
 _Static_assert(sizeof(arm_timeinfo_t) == 24, "ABI drift: arm_timeinfo_t");
+_Static_assert(offsetof(arm_timeinfo_t, timer_ticks) == 0,
+               "ABI drift: timeinfo timer_ticks offset");
+_Static_assert(offsetof(arm_timeinfo_t, scheduler_ticks) == 8,
+               "ABI drift: timeinfo scheduler_ticks offset");
+_Static_assert(offsetof(arm_timeinfo_t, scheduler_quantums) == 16,
+               "ABI drift: timeinfo scheduler_quantums offset");
 
 #endif
